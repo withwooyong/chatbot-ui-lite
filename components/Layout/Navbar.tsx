e1d94fae1d773cc4e66a1847fbd27cd7e63e814f
Ted <withwooyong@yanadoocorp.com>
2023-04-19 19:09:35 +0900

    Navbar modify

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import { ResetChat } from "../Chat/ResetChat";
 
-export const Navbar: FC<> = () => {
+export const Navbar: FC = () => {
   return (
     <>
       <div className="flex h-[50px] sm:h-[60px] border-b border-neutral-300 py-2 px-2 sm:px-8 items-center justify-between">
